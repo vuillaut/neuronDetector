453a4d967413b433dd8805a2345602d1f477f817
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2018-08-14 12:44:39 +0200

    update script to get events

--- scripts/neuron.py
+++ scripts/neuron.py
@@ -32,6 +32,42 @@ def plot_signal(signal, filename=None, ax=None, **kwargs):
     return ax
 
 
+def print_stats(df):
+    """
+    Print stats from a DataFrame and create a summary DataFrame that is returned
+    """
+    print("{} different experiments".format(len(set(df.experiment))))
+    print("{} detected neurons in total".format(len(df)))
+    print("{:.2f} neurons per experiment on average".format(len(df)/len(set(df.experiment))))
+
+    stat_df = pd.DataFrame()
+
+    print("")
+    exps = set(df.experiment)
+    divs = []
+    for exp in exps:
+        div = exp[exp.lower().find("div"):exp.lower().find("div")+5]
+        div = div.replace('_', '')
+        if div not in divs:
+            divs.append(div)
+
+    stats_df = pd.DataFrame(np.zeros((len(divs), 3)),
+                            columns=['nb_exp', 'nb_neur','nb_neur/nb_exp'],
+                            index=np.sort(divs))
+
+    for d in np.sort(divs):
+        nexp = len([exp for exp in exps if d in exp])
+        nneur = len(df[df['experiment'].str.contains(d)])
+        print("{}:".format(d))
+        print("{} experiments\n{} neurons\n{:.2f} neurons per exp\n".format(nexp, nneur, nneur/nexp))
+        stats_df['nb_exp'][d] = nexp
+        stats_df['nb_neur'][d] = nneur
+        stats_df['nb_neur/nb_exp'][d] = nneur/nexp
+
+    return stats_df
+
+
+
 def plot_relative_signal(signal, filename=None, ax=None, **kwargs):
     ax = plt.gca() if ax is None else ax
 
@@ -64,8 +100,8 @@ def find_events(video, limit=1.1):
     n = 0
     current_strike = False
     args = []
-    m = signal.sum(axis=(1,2)).mean()
-    for ii, image in enumerate(signal):
+    m = video.sum(axis=(1,2)).mean()
+    for ii, image in enumerate(video):
         if image.sum()/m > limit and current_strike is False :
             args.append(ii)
             n += 1
@@ -75,3 +111,11 @@ def find_events(video, limit=1.1):
         else:
             current_strike = False
     return n, np.array(args)
+
+
+def get_peak_hist(df, exp=None):
+    if exp is None:
+        mask = np.ones(len(df), dtype=bool)
+    else:
+        mask = df.experiment==exp
+    return np.histogram(df[mask].Z, bins=np.linspace(10,305,30))[0]
